c4a88b5e791d29d300b083df9d9f47f89b3c3468
edandaniel <logonrmlocal@fiap.com.br>
2018-12-04 21:12:34 -0200

    add firebase stuff

--- app/src/main/java/com/edandaniel/jokenpokemon/view/LoginActivity.kt
+++ app/src/main/java/com/edandaniel/jokenpokemon/view/LoginActivity.kt
@@ -3,8 +3,10 @@ package com.edandaniel.jokenpokemon.view
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.edandaniel.jokenpokemon.R
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
