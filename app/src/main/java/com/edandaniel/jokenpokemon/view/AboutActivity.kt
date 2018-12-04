771fb3664093caa2aaee15473646f79cbba12e31
edandaniel <logonrmlocal@fiap.com.br>
2018-12-04 20:24:33 -0200

    add all app screens

---
+++ app/src/main/java/com/edandaniel/jokenpokemon/view/AboutActivity.kt
@@ -0,0 +1,13 @@
+package com.edandaniel.jokenpokemon.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.edandaniel.jokenpokemon.R
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
---
+++ app/src/main/java/com/edandaniel/jokenpokemon/view/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.edandaniel.jokenpokemon.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.edandaniel.jokenpokemon.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
