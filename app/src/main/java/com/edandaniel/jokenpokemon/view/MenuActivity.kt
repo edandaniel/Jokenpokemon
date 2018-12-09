63cf67367cbedef47470b117a6c21a011d8fdf79
jafferveronezi <jafferveronezi@gmail.com>
2018-12-09 18:55:02 -0200

    adiciona funbções para os botões de menu e corrige layout

--- app/src/main/java/com/edandaniel/jokenpokemon/view/MenuActivity.kt
+++ app/src/main/java/com/edandaniel/jokenpokemon/view/MenuActivity.kt
@@ -1,13 +1,36 @@
 package com.edandaniel.jokenpokemon.view
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.edandaniel.jokenpokemon.R
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        btJogar.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+        }
+
+        btRanking.setOnClickListener {
+            val intent = Intent(this, RankingActivity::class.java)
+            startActivity(intent)
+        }
+
+        btSobre.setOnClickListener {
+            val intent = Intent(this, AboutActivity::class.java)
+            startActivity(intent)
+        }
+
+        btSair.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
